07a24da369ce1510efe9fb87d101beb1dde9ca96
minhkhoiv12 <khoitk14033@gmail.com>
2025-05-01 21:11:25 +0700

    Add friend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,20 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Messenger from "./components/Messenger";
 
 function App() {
   return (
     <div>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/messenger/login" element={<Login />} />
-        <Route path="/messenger/register" element={<Register />} /> 
-      </Routes>
-  </BrowserRouter>,
-    
-  </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/messenger/login" element={<Login />} />
+          <Route path="/messenger/register" element={<Register />} />
+          <Route path="/" element={<Messenger />} />
+        </Routes>
+      </BrowserRouter>
+      ,
+    </div>
   );
 }
 
